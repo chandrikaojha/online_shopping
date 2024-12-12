9b9d15ca40b252381cd00d16ea42a118037c80bb
chandrikaojha <chandrikaojha079@gmail.com>
2024-12-12 09:49:15 +0545

    title change

--- home.php
+++ home.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Bootstrap Example</title>
+  <title>home</title>
   
   <!-- Bootstrap CSS -->
   <link 
